8fdeecaaa39a50da4d36210444ce5fdb2f4a122a
Guust207 <92439850+Guust207@users.noreply.github.com>
2025-03-04 10:52:17 +0100

    Implemented hit for hand class

--- Game/hand.py
+++ Game/hand.py
@@ -1,11 +1,14 @@
+from card import Card, Suit
+
 class Hand:
     def __init__(self, hand_cards):
         self.hand_cards = hand_cards
         self.insurance = False
         self.double_down = False
 
-    def hit(self):
-        # logic for hitting
+    def hit(self, available_cards):
+        # Remove top card from game deck and add to current hand
+        self.hand_cards.append(available_cards.pop())
         return
 
     def stand(self):
@@ -26,3 +29,30 @@ class Hand:
 
     def get_hand_cards(self):
         return self.hand_cards
+
+"""
+# Test for hitting
+
+deck = [Card(Suit.CLUBS, 10), Card(Suit.HEARTS, 2), Card(Suit.DIAMONDS, 7), Card(Suit.SPADES, 7)]
+current_hand = Hand([])
+
+print("Available cards before:")
+for i in range(len(deck)):
+    suit, value = deck[i].get_card()
+    print(f"Card {i}: {suit.name}, {value}")
+
+current_hand.hit(deck)
+current_hand.hit(deck)
+current_hand.hit(deck)
+
+print("\nAvailable cards after:")
+for i in range(len(deck)):
+    suit, value = deck[i].get_card()
+    print(f"Card {i}: {suit.name}, {value}")
+
+print("\nFinal hand:")
+for i in range(len(current_hand.get_hand_cards())):
+    suit, value = current_hand.get_hand_cards()[i].get_card()
+    print(f"Card {i}: {suit.name}, {value}")
+
+"""
