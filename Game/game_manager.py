979efc43c067633c7c27ff8983a77a51cb3a2652
Arvin Rapstad <101880810+Athiene@users.noreply.github.com>
2025-03-14 13:33:06 +0100

    Fixed and improved game_manager_class for integration

--- Game/game_manager.py
+++ Game/game_manager.py
@@ -1,44 +1,24 @@
 from Game.card import Card
-from Game.dealer import Dealer
 import random
+from Game.card import Suit
+from Game.dealer import Dealer
 
 class GameManager:
-
     # On initialize
     def __init__(self, deck_count):
-        self.availalbe_cards = []
-        self.dealer = Dealer()
         self.players = []
+        self.available_cards = []
+        self.dealer = Dealer()
 
-        # Creates the stack of cards based on deck_count
+        # Sets up initial game setup, i.e Dealer gets 2 cards and player(s) get 2 cards
+        self.initial_setup(deck_count)
+
+    def initialSetup(self, deck_count):
+        # Create deck of cards, based on the amount of decks (deck_count) you want
         self.create_decks(deck_count=deck_count)
 
         # Shuffles the deck at random
         self.shuffle()
-
-        # Sets up initial game setup, i.e Dealer gets 2 cards and player(s) get 2 cards
-        self.initialSetup():
-
-        # # For testing
-        # for card in self.availalbe_cards:
-        #     suit, value = card.get_card()
-        #     print(f"Suit: {suit}, Value: {value}")
-        # print("####### AFTER SHUFFLE #######")
-        # # For testing
-        # for card in self.availalbe_cards:
-        #     suit, value = card.get_card()
-        #     print(f"Suit: {suit}, Value: {value}")
-
-    def shuffle(self):
-        random.shuffle(self.availalbe_cards)
-
-    def create_decks(self, deck_count):
-        for i in range(deck_count):
-            for suit in ["S", "D", "C", "H"]:
-                self.availalbe_cards.extend(Card(suit, n+1) for n in range(13))
-            return
-
-    def initialSetup():
         return
 
     def get_dealer_cards(self):
@@ -49,4 +29,12 @@ class GameManager:
 
     def get_player_cards(self):
         return self.players
-
+    
+    def create_decks(self, deck_count):
+        for i in range(deck_count):
+            for suit in [1, 2, 3, 4]:
+                self.available_cards.extend(Card(Suit(suit), n+1) for n in range(13))
+            return
+        
+    def shuffle(self):
+        random.shuffle(self.availalbe_cards)
