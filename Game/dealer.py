5e4df4664eecca16b98032dc0a268cc91b9d6416
Arvin Rapstad <101880810+Athiene@users.noreply.github.com>
2025-03-14 13:17:41 +0100

    Implemented improvements

--- Game/dealer.py
+++ Game/dealer.py
@@ -4,15 +4,42 @@ import random
 class Dealer:
     def __init__(self):
         self.dealer_cards = []
+        self.possible_values = []
+        self.calculate_hand_values()
         return
 
     def hit(self, availalbe_cards):
         hit_card = availalbe_cards.pop()
         self.dealer_cards.append(hit_card)
+        self.calculate_hand_values()
         return hit_card
 
     def stand(self):
         return
 
     def get_dealer_cards(self):
-        return self.dealer_cards
+        self.calculate_hand_values()
+        return self.possible_values
+
+    def calculate_hand_values(self):
+        total_values = [0]
+        for card in self.dealer_cards:
+            # Get last element which is the card value
+            card_value = card.get_card()[-1] 
+
+            # NOT ACE: Simply add card value to all possible total values of hand
+            if card_value > 1:
+                add_value = min(card_value, 10)
+                for i in range(len(total_values)):
+                    total_values[i] += add_value
+                continue
+
+            # ACE: Add 1 to all possible total values of hand, and add a new entry with the highest of the values + 11
+            max_value = max(total_values)
+            print("Test")
+            for i in range(len(total_values)):
+                total_values[i] += 1
+            total_values.append(max_value + 11)
+
+        self.possible_values = total_values
+        return
