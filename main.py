f1277d8782708e27109151419c7ec70cd9340e69
Arvin Rapstad <101880810+Athiene@users.noreply.github.com>
2025-03-28 15:03:35 +0100

    Implemented so basic features for main.py that uses the enviroment.

--- main.py
+++ main.py
@@ -5,25 +5,33 @@ from Game.card import Card, Suit
 enviroment = Enviroment(deck_count=1)
 
 def print_enviroment_state():
-    available_cards, players, dealer = enviroment.get_game_state()
-    print(f'Total available cards: {len(available_cards)}')
     print("### Players ###")
-    for player_index, player in enumerate(players):  # Track player index
+    for player_index, player in enumerate(enviroment.get_game_manager().get_players()):  # Track player index
         print(f'- Player {player_index+1}')
         for hand_index, hand in enumerate(player.get_hands()):
             print(f'    - Hand #{hand_index+1}')
             for card in hand.get_hand_cards():
                 print(f'        {card.suit}, Value: {card.value}')
             print(f'        Possible values: {hand.get_possible_values()}')
-    print('### Dealer ###')
-    for card in dealer.get_dealer_cards():
+            print(f'        Hand stake: {hand.get_hand_stake()}')
+            print('### Dealer ###')
+    for card in enviroment.get_game_manager().get_dealer().get_dealer_cards():
             print(f'    {card.suit}, Value: {card.value}')
-    print(f'    Possible values: {dealer.get_possible_values()}')
+    print(f'    Possible values: {enviroment.get_game_manager().get_dealer().get_possible_values()}')
+    print("### Overviews ###")
+    print(f'Total available cards: {len(enviroment.get_game_manager().get_available_cards())}')
 
-def action_round()
+#Test
+def moves():
+    for player_index, player in enumerate(enviroment.get_game_manager().get_players()): 
+        for hand_index, hand in enumerate(player.get_hands()):
+            if (min(10, hand.get_hand_cards()[0].value) == min(10, hand.get_hand_cards()[1].value)):
+                player.action_input(hand_index, 3, enviroment.get_game_manager().get_available_cards())
+
 
 print_enviroment_state()
-
+moves()
+print_enviroment_state()
     
 
 
