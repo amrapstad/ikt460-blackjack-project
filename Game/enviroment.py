d27e847f9adbbc7196c6f78413021f7d13f0610c
Arvin Rapstad <101880810+Athiene@users.noreply.github.com>
2025-04-01 15:54:06 +0200

    Integration related code for #20

--- Game/enviroment.py
+++ Game/enviroment.py
@@ -6,6 +6,10 @@ class Enviroment:
 
     def get_game_manager(self):
         return self.game_manager
-
+        
     def get_game_state(self):
         return self.game_manager.get_available_cards(), self.game_manager.get_players(), self.game_manager.get_dealer()
+
+    def input(self, player_index, hand_index, action):
+        self.get_game_manager().play_episode(player_index, hand_index, action)
+        return
