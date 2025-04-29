f469351500b0f9d8028e3c5779043b133a200886
Guust207 <92439850+Guust207@users.noreply.github.com>
2025-04-29 11:07:42 +0200

    Fixed bug of hand total getting converted to 10
    Separated the functions for retrieving actions from split and regular totals respectively. Also added path to CSV and Plots directory in definitions.py.

--- Agent/optimal_agent.py
+++ Agent/optimal_agent.py
@@ -2,26 +2,24 @@ import pandas as pd
 import os
 from definitions import TABLES_DIR
 
-def get_action_from_csv(csv_table, player_card_value, face_up_card):
-    # Format:
-    ## First row : Face up card value
-    ## First column: Hand total (each card value in split case)
-
-    # Soft/hard total return values:
-    ## S - Stand
-    ## H - Hit
-    ## D - Double down
-
-    # Split return values:
-    # Y - Yes, do split
-    # N, No, don't do split
-
+def get_split_action_from_csv(csv_table, player_card_value, face_up_card):
     # Make jack, queen and king equal 10
     player_card_value = min(player_card_value, 10)
     face_up_card = min(face_up_card, 10)
 
     # First column name is '0'
     row = csv_table[csv_table['0'] == player_card_value]
+
+    value = row[str(face_up_card)].values[0]
+
+    return value
+
+def get_total_action_from_csv(csv_table, hand_total, face_up_card):
+    # Make jack, queen and king equal 10
+    face_up_card = min(face_up_card, 10)
+
+    # First column name is '0'
+    row = csv_table[csv_table['0'] == hand_total]
 
     value = row[str(face_up_card)].values[0]
 
@@ -53,25 +51,16 @@ class OptimalAgent:
         return
 
     def choose_action(self, player_hand, dealer_face_up_card):
-        # Get player hand
-        # Is it splittable?
-        ## Y: Perform split and end.
-        ## N: Continue
-        # Is the hand hard or soft?
-        ## Soft: Perform action from soft table
-        ## Hard: Perform action from hard table
-
-        card_list = player_hand.hand_cards
         face_up_card_value = dealer_face_up_card.value
 
         current_action = -1
 
         # Check for split
-        if len(card_list) == 2:
-            card1_value = min(card_list[0].value, 10)
-            card2_value = min(card_list[1].value, 10)
+        if len(player_hand.hand_cards) == 2:
+            card1_value = min(player_hand.hand_cards[0].value, 10)
+            card2_value = min(player_hand.hand_cards[1].value, 10)
             if card1_value == card2_value:
-                result = get_action_from_csv(self.split_table, card1_value, face_up_card_value)  # Y or N
+                result = get_split_action_from_csv(self.split_table, card1_value, face_up_card_value)  # Y or N
                 current_action = self.action_dictionary[result]
 
         # When not splitting, whether possible or not
@@ -81,15 +70,15 @@ class OptimalAgent:
 
             if soft_hand:
                 hand_total = max(player_hand.possible_values)
-                result = get_action_from_csv(self.soft_total_table, hand_total, face_up_card_value)
+                result = get_total_action_from_csv(self.soft_total_table, hand_total, face_up_card_value)
                 current_action = self.action_dictionary[result]
             else:
                 hand_total = player_hand.possible_values[0]
-                result = get_action_from_csv(self.hard_total_table, hand_total, face_up_card_value)
+                result = get_total_action_from_csv(self.hard_total_table, hand_total, face_up_card_value)
                 current_action = self.action_dictionary[result]
 
         # Lastly, convert DD to hit if more than 2 cards in hand
-        if len(card_list) > 2 and current_action == 2:
+        if len(player_hand.hand_cards) > 2 and current_action == 2:
             current_action = 1
 
         if current_action < 0 or current_action > 3:
--- definitions.py
+++ definitions.py
@@ -2,4 +2,6 @@ import os
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
 
-TABLES_DIR =os.path.join(ROOT_DIR, "OptimalTables")
+TABLES_DIR = os.path.join(ROOT_DIR, "OptimalTables")
+CSV_DIR = os.path.join(ROOT_DIR, "CSV")
+PLOTS_DIR = os.path.join(ROOT_DIR, "Plots")
