1453f5b88536e1404e9920abecc4409f0582a8c5
Arvin Rapstad <arvinrapstad@gmail.com>
2025-05-20 18:01:06 +0200

    Convergence config

--- main.py
+++ main.py
@@ -52,24 +52,28 @@ if __name__ == "__main__":
 
     # Q-agent training
     current_pos = 0
-    q_learning_result = run_simulation_q_learning(num_players=4, q_agent_pos=current_pos, rounds_to_simulate=1000)
-    q_learning_agent = q_learning_result[current_pos] # q_learning_result[q_agent_pos]
+    q_learning_result = run_simulation_q_learning(num_players=4, q_agent_pos=current_pos, rounds_to_simulate=100000)
+    q_learning_agent = q_learning_result[current_pos]
+    plot_state_value_heatmaps(q_learning_agent) 
 
-    # MBVE Q-agent training
-    # TODO: Train a q-agent with mbve
+    # MBVE training
+    current_pos = 0
+    mbve_learning_result = run_simulation_q_learning(num_players=4, q_agent_pos=current_pos, with_mbve=True, rounds_to_simulate=100000)
+    mbve_learning_agent = mbve_learning_result[current_pos]
+    plot_state_value_heatmaps(mbve_learning_agent) 
 
     # Training results
-    plot_training_results(q_learning_result)
-    plot_action_distribution(q_learning_result)
-    plot_state_value_heatmaps(q_learning_agent)
-    plot_q_value_convergence(q_learning_agent, window_size=100)
+    plot_q_value_convergence(q_learning_agent, window_size=1000)
+    plot_q_value_convergence(mbve_learning_agent, window_size=1000)
+    plot_training_results(q_learning_result, window_size=1000)
+    plot_training_results(mbve_learning_result, window_size=1000)
 
     # Evaluation
-    eval_players = [q_learning_agent, OptimalAgent(), RandomAgent()]
-    run_evaluation(eval_players, num_games=1000)
-    plot_evaluation_results(eval_players)
+    eval_players = [OptimalAgent(), q_learning_agent, mbve_learning_agent, RandomAgent()]
+    run_evaluation(eval_players, num_games=10000)
+    plot_evaluation_results(eval_players, window_size=500)
 
-    # Training vs. Evaluation action distribution
-    plot_return_distributions(eval_players)
+    # Other results
+    plot_action_distribution(eval_players)
 
     print("The end")
